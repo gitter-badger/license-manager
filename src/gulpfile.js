da7f24785a7dcf7d8d5b001f3e05e3105261fac5
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2016-02-25 21:23:01 +0100

    Poprawki w plikach konfiguracyjnych

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,1 +1,7 @@
 var gulp = require('gulp');
+var gulp_concat = require('gulp-concat');
+var gulp_cssmin = require('gulp-cssmin');
+var gulp_uglify = require('gulp-uglify');
+var rimraf = require('rimraf');
+
+var webroot = "./wwwroot/";

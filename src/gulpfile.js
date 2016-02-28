32c7d04a65f7f34693d5a005d5571dbd6243e035
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2016-02-28 21:59:09 +0100

    Automatyczne generowanie plików CSS na podstawie plików LESS

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,7 +1,31 @@
-var gulp = require('gulp');
-var gulp_concat = require('gulp-concat');
-var gulp_cssmin = require('gulp-cssmin');
-var gulp_uglify = require('gulp-uglify');
-var rimraf = require('rimraf');
+var gulp = require("gulp");
+var gulp_concat = require("gulp-concat");
+var gulp_cssmin = require("gulp-cssmin");
+var gulp_less = require("gulp-less");
+var gulp_rename = require("gulp-rename");
+var gulp_uglify = require("gulp-uglify");
+var rimraf = require("rimraf");
 
-var webroot = "./wwwroot/";
+var paths = {
+    webroot: "./wwwroot/",
+    css: "./wwwroot/css/",
+    js: "./wwwroot/js/",
+    less: "./Styles/"
+};
+
+gulp.task("less", function () {
+    return gulp
+        .src(paths.less + "*.less")
+        .pipe(gulp_less())
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task("min:css", ["less"], function () {
+    return gulp
+        .src([paths.css + "*.css", "!" + paths.css + "*.min.css"])
+        .pipe(gulp_cssmin())
+        .pipe(gulp_rename( { suffix: ".min" }))
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task("css", ["min:css"])
